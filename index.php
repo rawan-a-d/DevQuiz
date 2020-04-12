7d80501405f79ae4902ce6d651ef9e8d2087aff5
Rawan Abou Dehn <r.aboudehn@student.fontys.nl>
2020-04-12 02:23:23 +0200

    Add JS animation to index page

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php 
-
 	/* Check session */
 	include('config/session.php');
 
@@ -18,7 +17,7 @@
 	<?php include('templates/header.php') ?>
 
 	<!-- Header -->
-	<header  id="website_purpose">
+	<header id="website_purpose">
 		<h2>Hello <?php echo $_SESSION['username'] ;?></h2>
 		<h2>Welcome to DEVQUIZ</h2>
 		<h4>The most comprehensive resource with a lot of quality programming quiz, questions and tutorials that help you to improve you programming knowledge.</h4>
@@ -27,13 +26,21 @@
 	<!-- Main tag(includes explaination about technologies and some examples) -->
 	<main id="main">
 		<!-- Explaination HTML -->
-		<div class="technology_explanation box" id="box_html_explanation">
-			<h4>HTML (HyperText Markup Language)</h4>
-			the code that is used to structure a web page and its content. For example, content could be structured within a set of paragraphs, a list of bulleted points, or using images and data tables. As the title suggests, this article will give you a basic understanding of HTML and its functions.
-			<!-- Level buttons -->
-			<div>
-				<button id="level1">Beginner</button>
-				<button id="level2">Intermediate</button>
+		<div class="card-container technology_explanation box" id="box_html_explanation">
+			<div class="card">
+			    <div class="front">
+					<h4>HTML (HyperText Markup Language)</h4>
+				</div>
+				<div class="back">
+					<p>
+					The code that is used to structure a web page and its content. For example, content could be structured within a set of paragraphs, a list of bulleted points, or using images and data tables. As the title suggests, this article will give you a basic understanding of HTML and its functions.
+					</p>
+					<!-- Level buttons -->
+					<div>
+						<button id="level1">Beginner</button>
+						<button id="level2">Intermediate</button>
+					</div>
+				</div>
 			</div>
 		</div>
 		<!-- Example HTML -->
@@ -49,15 +56,22 @@
 		</div>
 
 		<!-- Explaination CSS -->
-		<div class="technology_explanation box" id="box_css_explanation">
-			<h4>CSS (Cascading Style Sheets)</h4>
-
-			a style sheet language that specifies how documents are presented to users — how they are styled, laid out
-			CSS can be used for very basic document text styling — for example changing the color and size of headings and links. It can be used to create layout 
-			<!-- Level buttons -->
-			<div>
-				<button id="level1">Beginner</button>
-				<button id="level2">Intermediate</button>
+		<div class="card-container technology_explanation box" id="box_css_explanation">
+			<div class="cardCSS">
+			    <div class="front">
+					<h4>CSS (Cascading Style Sheets)</h4>
+				</div>
+				<div class="back">
+					<p>
+						A style sheet language that specifies how documents are presented to users — how they are styled, laid out
+						CSS can be used for very basic document text styling — for example changing the color and size of headings and links. It can be used to create layout
+					</p>
+					<!-- Level buttons -->
+					<div>
+						<button id="level1">Beginner</button>
+						<button id="level2">Intermediate</button>
+					</div>
+				</div>
 			</div>
 		</div>
 		<!-- Example CSS -->
@@ -75,13 +89,21 @@
 		</div>
 
 		<!-- Explaination JavaScript -->
-		<div class="technology_explanation box" id="box_js_explanation">
-			<h4>JavaScript</h4>
-			a programming language that adds interactivity to your website (for example games, responses when buttons are pressed or data is entered in forms, dynamic styling, and  animation). This article helps you get started with this exciting language and gives you an idea of what is possible.
-			<!-- Level buttons -->
-			<div>
-				<button id="level1">Beginner</button>
-				<button id="level2">Intermediate</button>
+		<div class="card-container technology_explanation box" id="box_js_explanation">
+			<div class="cardJS">
+			    <div class="front">
+					<h4>JavaScript</h4>
+				</div>
+				<div class="back">				
+					<p>
+						A programming language that adds interactivity to your website (for example games, responses when buttons are pressed or data is entered in forms, dynamic styling, and  animation). This article helps you get started with this exciting language and gives you an idea of what is possible.				
+					</p>
+					<!-- Level buttons -->
+					<div>
+						<button id="level1">Beginner</button>
+						<button id="level2">Intermediate</button>
+					</div>
+				</div>
 			</div>
 		</div>
 		<!-- Example JavaScript -->
@@ -99,4 +121,7 @@
 
 	<!-- Include footer -->
 	<?php include('templates/footer.php') ?>
+
+	<script type="text/javascript" src="js/anime.min.js"></script>
+	<script type="text/javascript" src="js/index.js"></script>
 </html>
